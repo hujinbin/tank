e93a33d1306aeaa863e21e1f829fdfe69a591c45
hujinbin <437044048@qq.com>
2019-12-11 17:37:53 +0800

    edit

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3030, function(){
+  console.log('listening on *:3030');
 });
